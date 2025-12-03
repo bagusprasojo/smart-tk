fc3e5b81176f9bdebb67112c5bc8a9caa522875f
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2025-12-03 13:03:20 +0700

    ubah setting.py

--- smarttk/settings.py
+++ smarttk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9s-xdc=-p$i6as=b=2=b1dhm^&59wo5!vu7360nul=r96t2gqi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','myapp01.pythonanywhere.com']
 
 
 # Application definition
